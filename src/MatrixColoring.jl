d92b2fc356de18a5aa05571631efe9725de6c2e8
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-03-28 01:30:26 +0530

    main package file

---
+++ src/MatrixColoring.jl
@@ -0,0 +1,10 @@
+module MatrixColoring
+
+export CGraph, add_vertex!, add_edge!, neighbors, vertices, has_edge, num_edges, has_vertex, rem_edge!, rem_vertex!,
+colorGraph, num_vertices, max_degree_vertex, non_neighbors, length_common_neighbor, vertex_degree, contract!
+
+include("custom_graph.jl")
+include("contraction_algo.jl")
+
+
+end # module

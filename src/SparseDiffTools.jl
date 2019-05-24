83708b94a0d5cedebd1e058ecaaf7d25ba9e6a7f
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-05-24 18:09:07 +0530

    Renamed main file to reflect modified package name and changed syntax to include BSC coloring algo in the module as well.

--- src/SparseDiffTools.jl
+++ src/SparseDiffTools.jl
@@ -1,10 +1,11 @@
-module MatrixColoring
+module SparseDiffTools
 
 export CGraph, add_vertex!, add_edge!, neighbors, vertices, has_edge, num_edges, has_vertex, rem_edge!, rem_vertex!,
-colorGraph, num_vertices, max_degree_vertex, non_neighbors, length_common_neighbor, vertex_degree, contract!
+colorGraph, num_vertices, max_degree_vertex, non_neighbors, length_common_neighbor, vertex_degree, contract!, bsc_color
 
 include("custom_graph.jl")
 include("contraction_algo.jl")
+include("bsc_algo.jl")
 
 
 end # module

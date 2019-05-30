250aadc6d5316b21a92cb1a8ff43346fdc9830f7
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-05-30 23:30:35 +0530

    updated matrix2graph.jl with sparse array
    Used SparseArrays.jl to change input argument type from simple matrix to SparseMatrixCSC

--- src/matrix2graph.jl
+++ src/matrix2graph.jl
@@ -1,3 +1,4 @@
+using SparseArrays
 include("custom_graph.jl")
 
 """
@@ -6,45 +7,34 @@ include("custom_graph.jl")
 A utility function to generate a graph from input
 sparse matrix, columns are represented with vertices
 and 2 vertices are connected with an edge only if
-the two columns are not mutually orthogonal.
+the two columns are mutually orthogonal.
 """
-function matrix2graph(SparseMatrix::Array{Int64,2})
+function matrix2graph2(SparseMatrix::SparseMatrixCSC{Int64,Int64})
+    dropzeros(SparseMatrix)
+    (rows_index, cols_index, val) = findnz(SparseMatrix)
+
     V = col = size(SparseMatrix, 2)
     row = size(SparseMatrix, 1)
 
-    vertices = zeros(Int64,0)
+    vertices = zeros(Int64, 0)
     edges = [Vector{Int64}() for _ in 1:V]
-    graph = CGraph(vertices, edges)
 
+    graph = CGraph(vertices, edges)
     for i = 1:V
-        push!(vertices,i)
+        push!(vertices, i)
     end
 
-    for i = 1:V
+    for i = 1:length(cols_index)
+        cur_col = cols_index[i]
         for j = 1:(i-1)
-            col1 = SparseMatrix[:,i]
-            col2 = SparseMatrix[:,j]
-            colRes = element_or(col1, col2)
-            oneMatrix = ones(Int64, row)
-            if oneMatrix' * colRes > 0
-                add_edge!(graph, i, j)
+            next_col = cols_index[j]
+            if cur_col != next_col
+                if row_index[i] == row_index[j]
+                    #add edge
+                    add_edge!(graph, i, j)
+                end
             end
         end
     end
     return graph
-end
-
-function element_or(col1, col2)
-    c3 = zeros(Int64,0)
-    for i = 1:length(col1)
-        x1 = col1[i]
-        y = col2[i]
-        if x1 != 0 && y != 0
-            res = 1
-        else
-            res = 0
-        end
-        push!(c3, res)
-    end
-    return c3
 end

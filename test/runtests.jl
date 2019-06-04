e3365e53c3487251738aa9f8bb931ad21882b9bd
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-06-05 01:08:08 +0530

    added test for matrix2graph

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using SparseDiffTools
 using Test
 
 
-@testset "Contraction Algorithm" begin include("test_contraction_algo.jl") end
 @testset "Greedy D1 Algorithm" begin include("test_greedy_d1.jl") end
+@testset "Matrix2Graph" begin include("test_matrix2graph.jl") end

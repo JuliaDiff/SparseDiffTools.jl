b3d26f7a035f4603c31287ced3ad1c358aea8593
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-06-04 12:47:29 +0530

    added new tests for greedy coloring

--- test/test_greedy_d1.jl
+++ test/test_greedy_d1.jl
@@ -1,12 +1,32 @@
 using SparseDiffTools
-include("test_data.jl")
+using VertexSafeGraphs
+using LightGraphs
+using Random
 
-for i = 1:5
-    g = graphs[i]
+test_graphs = Array{VSafeGraph, 1}(undef, 0)
+
+for _ in 1:20
+    nv = rand(5:25)
+    ne = rand(1: nv^2)
+    inner = SimpleGraph(nv)
+    graph = VSafeGraph(inner)
+    for e in 1:ne
+        v1 = rand(1:nv)
+        v2 = rand(1:nv)
+        while v1 == v2
+            v2 = rand(1:nv)
+        end
+        add_edge!(graph, v1, v2)
+    end
+    push!(test_graphs, copy(graph))
+end
+
+for i in 1:20
+    g = test_graphs[i]
     out_colors = greedy_d1(g)
-    for v = 1:length(g.vertices)
+    for v = 1:nv(g)
         color = out_colors[v]
-        for j in neighbors(g, v)
+        for j in inneighbors(g, v)
             if out_colors[j] == color
                  @test false
             end

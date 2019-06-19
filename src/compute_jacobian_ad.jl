87c348b0b7d247b7a7d745788f5ec108e41c118b
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-06-19 05:09:18 -0400

    create a cache type for holding the dual caches

--- src/compute_jacobian_ad.jl
+++ src/compute_jacobian_ad.jl
@@ -1,35 +1,71 @@
 using ForwardDiff: Dual, jacobian, partials
 
-function compute_jacobian!(J::AbstractMatrix{<:Number},
-                f,
-                x::AbstractArray{T};
-                color=1:length(x)) where {T<:Number}
+struct ForwardColorJacCache{T,T2,T3,T4,T5}
+    t::T
+    fx::T2
+    dx::T3
+    p::T4
+    color::T5
+end
 
-    t = zeros(Dual{typeof(f), Float64, maximum(color)},length(x))
-    fx = similar(t); du = similar(x)
-    partials_array = Array{T}(undef, length(x), maximum(color))
+function ForwardColorJacCache(f,x;
+                              dx = nothing,
+                              color=1:length(x))
 
+    t = zeros(Dual{typeof(f), eltype(x), maximum(color)},length(x))
+
+    if dx === nothing
+        fx = similar(t)
+        _dx = similar(x)
+    else
+        fx = zeros(Dual{typeof(f), eltype(dx), maximum(color)},length(dx))
+        _dx = dx
+    end
+
+    partials_array = Array{Bool}(undef, length(x), maximum(color))
     for color_i in 1:maximum(color)
         for i in 1:length(x)
             if color[i]==color_i
-                partials_array[i,color_i] = 1
+                partials_array[i,color_i] = true
             else
-                partials_array[i,color_i] = 0
+                partials_array[i,color_i] = false
             end
         end
     end
-
     p = Tuple.(eachrow(partials_array))
+
+    ForwardColorJacCache(t,fx,_dx,p,color)
+end
+
+function forwarddiff_color_jacobian!(J::AbstractMatrix{<:Number},
+                f,
+                x::AbstractArray{<:Number};
+                dx = nothing,
+                color)
+    forwarddiff_color_jacobian!(J,f,x,ForwardColorJacCache(f,x,dx=dx,color=color))
+end
+
+function forwarddiff_color_jacobian!(J::AbstractMatrix{<:Number},
+                f,
+                x::AbstractArray{<:Number},
+                jac_cache = ForwardColorJacCache(f,x))
+
+    t = jac_cache.t
+    fx = jac_cache.fx
+    dx = jac_cache.dx
+    p = jac_cache.p
+    color = jac_cache.color
+
     t .= Dual{typeof(f)}.(x, p)
     f(fx, t)
 
     if J isa SparseMatrixCSC
         rows_index, cols_index, val = findnz(J)
         for color_i in 1:maximum(color)
-            du .= partials.(fx,color_i)
+            dx .= partials.(fx,color_i)
             for i in 1:length(cols_index)
                 if color[cols_index[i]]==color_i
-                    J[rows_index[i],cols_index[i]] = du[rows_index[i]]
+                    J[rows_index[i],cols_index[i]] = dx[rows_index[i]]
                 end
             end
         end

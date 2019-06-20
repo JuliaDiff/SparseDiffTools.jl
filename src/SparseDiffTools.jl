7d56482e3602b853a751b01af51f12b9833844e6
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-20 14:09:05 -0400

    add high level interface

--- src/SparseDiffTools.jl
+++ src/SparseDiffTools.jl
@@ -1,14 +1,19 @@
 module SparseDiffTools
 
+using SparseArrays, LinearAlgebra, LightGraphs, VertexSafeGraphs
+using ForwardDiff: Dual, jacobian, partials, DEFAULT_CHUNK_THRESHOLD
+
 export  contract_color,
         greedy_d1,
         matrix2graph,
+        matrix_colors,
         forwarddiff_color_jacobian!,
         ForwardColorJacCache
 
+include("coloring/high_level.jl")
 include("coloring/contraction_coloring.jl")
 include("coloring/greedy_d1_coloring.jl")
-include("differentiation/matrix2graph.jl")
+include("coloring/matrix2graph.jl")
 include("differentiation/compute_jacobian_ad.jl")
 
 end # module

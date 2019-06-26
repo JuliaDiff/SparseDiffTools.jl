344f6dbfeb02cfce4a9a7d72c3589d88ddbe70e7
Shashi Gowda <shashigowda91@gmail.com>
2019-06-26 15:25:55 -0400

    add branching tests

---
+++ test/program_sparsity/testall.jl
@@ -0,0 +1,4 @@
+include("common.jl")
+
+@testset "Basics" begin include("basics.jl") end
+@testset "Exploration" begin include("ifsandbuts.jl") end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,4 @@ using Test
 @testset "Exact coloring via contraction" begin include("test_contraction.jl") end
 @testset "Greedy distance-1 coloring" begin include("test_greedy_d1.jl") end
 @testset "Matrix to graph conversion" begin include("test_matrix2graph.jl") end
+@testset "Jacobian sparsity computation" begin include("program_sparsity/testall.jl") end

f62c164813a105eacb76574525ef0e04aa9b0cb2
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-07-02 04:56:18 +0530

    added function documentation

--- src/coloring/greedy_star1_coloring.jl
+++ src/coloring/greedy_star1_coloring.jl
@@ -6,6 +6,18 @@
     color using greedy approach. The number of colors
     used may be equal or greater than the chromatic
     number χ(G) of the graph.
+
+    A star coloring is a special type of distance - 1  coloring,
+    For a coloring to be called a star coloring, it must satisfy 
+    two conditions:
+
+    1. every pair of adjacent vertices receives distinct  colors
+    (a distance-1 coloring)
+
+    2. For any vertex v, any color that leads to a two-colored path 
+    involving v and three other vertices  is  impermissible  for  v.
+    In other words, every path on four vertices uses at least three
+    colors.
 """
 function greedy_star1_coloring(G::VertexSafeGraph, alg::GreedyStar1Coloring)
     V = nv(G), color = zeros(Int64, V)

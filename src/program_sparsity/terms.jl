ae9e4c6bb7d87ef0a2b0df2b2dd1fb7c2d042659
Shashi Gowda <shashigowda91@gmail.com>
2019-07-06 19:56:41 -0400

    optimize squaring with an equivalent expression

--- src/program_sparsity/terms.jl
+++ src/program_sparsity/terms.jl
@@ -13,7 +13,7 @@ Base.:+(comb1::TermCombination) = comb1
 function _merge(dict1, dict2)
     d = copy(dict1)
     for (k, v) in dict2
-        d[k] = get(dict1, k, 0) + v
+        d[k] = min(2, get(dict1, k, 0) + v)
     end
     d
 end
@@ -21,6 +21,14 @@ end
 function Base.:*(comb1::TermCombination, comb2::TermCombination)
     if comb1 === comb2 # squaring optimization
         terms = comb1.terms
+        # turns out it's enough to track
+        # a^2*b^2
+        # and a^2 + b^2 + ab
+        # have the same hessian sparsity
+        t = Dict(k=>2 for (k,_) in
+                 Iterators.flatten(terms))
+        TermCombination([t])
+        #=
         # square each term
         t1 = [Dict(k=>2 for (k,_) in dict)
               for dict in comb1.terms]
@@ -32,6 +40,7 @@ function Base.:*(comb1::TermCombination, comb2::TermCombination)
             end
         end
         TermCombination(vcat(t1, t2))
+        =#
     else
         vec([_merge(dict1, dict2)
                for dict1 in comb1.terms,

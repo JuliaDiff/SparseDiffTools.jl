7c38ec4c8e9378f4970a586cda5ffa8e69132a52
David Widmann <devmotion@users.noreply.github.com>
2019-07-19 23:46:58 +0200

    Measure time and allocations

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using SafeTestsets
 
-@safetestset "Exact coloring via contraction" begin include("test_contraction.jl") end
-@safetestset "Greedy distance-1 coloring" begin include("test_greedy_d1.jl") end
-@safetestset "Greedy star coloring" begin include("test_greedy_star.jl") end
-@safetestset "Matrix to graph conversion" begin include("test_matrix2graph.jl") end
-@safetestset "AD using color vector" begin include("test_ad.jl") end
-@safetestset "Integration test" begin include("test_integration.jl") end
-@safetestset "Special matrices" begin include("test_specialmatrices.jl") end
-@safetestset "Jac Vecs and Hes Vecs" begin include("test_jaches_products.jl") end
-@safetestset "Program sparsity computation" begin include("program_sparsity/testall.jl") end
+@time @safetestset "Exact coloring via contraction" begin include("test_contraction.jl") end
+@time @safetestset "Greedy distance-1 coloring" begin include("test_greedy_d1.jl") end
+@time @safetestset "Greedy star coloring" begin include("test_greedy_star.jl") end
+@time @safetestset "Matrix to graph conversion" begin include("test_matrix2graph.jl") end
+@time @safetestset "AD using color vector" begin include("test_ad.jl") end
+@time @safetestset "Integration test" begin include("test_integration.jl") end
+@time @safetestset "Special matrices" begin include("test_specialmatrices.jl") end
+@time @safetestset "Jac Vecs and Hes Vecs" begin include("test_jaches_products.jl") end
+@time @safetestset "Program sparsity computation" begin include("program_sparsity/testall.jl") end

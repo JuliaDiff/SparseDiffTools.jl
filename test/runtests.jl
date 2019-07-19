8564ce3391967f3940f1b845f3f32ee1e10a6214
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-07-19 18:38:43 +0530

    Update test/runtests.jl
    Co-Authored-By: Mathieu Besançon <mathieu.besancon@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,5 +10,5 @@ using Test
 @testset "AD using color vector" begin include("test_ad.jl") end
 @testset "Integration test" begin include("test_integration.jl") end
 @testset "Special matrices" begin include("test_specialmatrices.jl") end
-@testset "Jac Vecs and Hes Vecs" begin include("test_jaches_products.jl") end
+@testset "Jacobian Vecs and Hessian Vecs" begin include("test_jaches_products.jl") end
 @testset "Program sparsity computation" begin include("program_sparsity/testall.jl") end

b3839702bbb9b273b92d159765b488d2bb497634
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-07-19 17:31:19 +0530

    final code for BSC algorithm

--- src/coloring/backtracking_coloring.jl
+++ src/coloring/backtracking_coloring.jl
@@ -4,13 +4,13 @@ using LightGraphs
     color_graph(g::LightGraphs, ::BacktrackingColor)
 
 Returns a tight, distance-1 coloring of graph g
-using the minimum number of colors possible (the
-chromatic number of g, χ(g))
+using the minimum number of colors possible (i.e.
+the chromatic number of graph, χ(g))
 """
 function color_graph(g::LightGraphs.AbstractGraph, ::BacktrackingColor)
-    println("function called")
     v = nv(g)
 
+    #A is list of vertices in non-increasing order of degree
     A = sort_by_degree(g)
 
     #F is the coloring of vertices, 0 means uncolored
@@ -18,9 +18,7 @@ function color_graph(g::LightGraphs.AbstractGraph, ::BacktrackingColor)
     F = zeros(Int32, v)
     Fopt= zeros(Int32, v)
 
-    #start index
     start = 1
-    println("start = $start")
 
     #optimal color number
     opt = v + 1
@@ -33,106 +31,65 @@ function color_graph(g::LightGraphs.AbstractGraph, ::BacktrackingColor)
     colors = zeros(Int32, v)
 
     #set of free colors
-    U = zeros(Int32, 0)
+    U = zeros(Int, 0)
     push!(U, 1)
-    print("1. U = ")
-    println(U)
 
     #set of free colors of x
     freeColors = [Vector{Int64}() for _ in 1:v]
     freeColors[x] = copy(U)
 
     while (start >= 1)
-        #println("Running while loop, start = $start")
+
         back = false
         for i = start:v
-            #println("running for loop, i = $i")
             if i > start
-                #println("i > start, entering if block (1)")
                 x = uncolored_vertex_of_maximal_degree(A,F)
-                #println("x = $x")
                 U = free_colors(x, A, colors, F, g, opt)
-                #print("U = ")
-                #println(U)
                 sort!(U)
-                #println("sorted U = ")
-                #println(U)
             end
             if length(U) > 0
-                #println("length of U > 0, entering if block (2)")
                 k = U[1]
-                #println("k = $k")
                 F[x] = k
-                #println("set F[$x] = $k")
                 cp = F[x]
-                #println("F[$x] = $cp")
                 deleteat!(U, 1)
-                #print("U = ")
-                #println(U)
                 freeColors[x] = copy(U)
-                #print("freeColors[$x] = ")
-                #println(U)
                 if i==1
                     l = 0
-                    #println("l = 0")
                 else
                     l = colors[i-1]
-                    #println("l = $l")
                 end
                 colors[i] = max(k, l)
-                #ck = colors[i]
-                #println("colors[$i] = $ck")
             else
-                #println("else condition, block (3)")
                 start = i-1
-                #println("start = $start")
                 back = true
-                #println("back = true")
                 break
             end
         end
+
         if back
-            #println("back is true, if block (4)")
             if start >= 1
-                #println("start = $start >= 1, if block (5)")
                 x = A[start]
-                #println("x = $x")
                 F[x] = 0
-                #println("F[$x] = 0")
                 U = freeColors[x]
-                #print("U = ")
-                #println(U)
             end
         else
-            #println("else condition, back is not true block (6)")
             Fopt = copy(F)
-            #print("Fopt = ")
-            #println(Fopt)
             opt = colors[v-1]
-            #println("opt = $opt")
             i = least_index(F,A,opt)
-            #println("i = $i")
             start = i-1
-            #println("start = $start")
             if start < 1
-                #println("start < 1, so breaking")
                 break
             end
 
             #uncolor all vertices A[i] with i >= start
             uncolor_all!(F, A, start)
-            #println("uncoloring")
-            #try start+1 instead
+
             for i = 1:start+1
                 x = A[i]
                 U = freeColors[x]
+
                 #remove colors >= opt from U
-                #print("U = ")
-                #println(U)
-                #println("removing from U all colors >= $opt")
                 U = remove_higher_colors(U, opt)
-                #print("U = ")
-                #println(U)
                 freeColors[x] = copy(U)
             end
         end
@@ -140,13 +97,14 @@ function color_graph(g::LightGraphs.AbstractGraph, ::BacktrackingColor)
     end
 
     Fopt
+
 end
 
 """
-    sort_by_degree()
+    sort_by_degree(g::LightGraphs.AbstractGraph)
 
-sort and store the vertices of graph g in
-non-increasing order of their degrees
+Returns a list of the vertices of graph g sorted
+in non-increasing order of their degrees
 """
 function sort_by_degree(g::LightGraphs.AbstractGraph)
     vs = vertices(g)
@@ -157,12 +115,12 @@ function sort_by_degree(g::LightGraphs.AbstractGraph)
 end
 
 """
-    uncolored_vertex_of_maximal_degree(A,F)
+    uncolored_vertex_of_maximal_degree(A::Array{Int,1},F::Array{Int,1})
 
-Returns an uncolored vertex from the graph which has maximum
-degree
+Returns an uncolored vertex from the partially
+colored graph which has the highest degree
 """
-function uncolored_vertex_of_maximal_degree(A,F)
+function uncolored_vertex_of_maximal_degree(A::Array{Int,1},F::Array{Int,1})
     for v in A
         if F[v] == 0
             return v
@@ -172,12 +130,22 @@ end
 
 
 """
-    free_colors()
+    free_colors(x::Int,
+                A::Array{Int,1},
+                colors::Array{Int,1},
+                F::Array{Int64,1},
+                g::LightGraphs.AbstractGraph,
+                opt::Int)
 
-returns set of free colors of x which are less
+Returns set of free colors of x which are less
 than optimal color number (opt)
 """
-function free_colors(x, A, colors, F, g, opt)
+function free_colors(x::Int,
+                    A::Array{Int,1},
+                    colors::Array{Int,1},
+                    F::Array{Int64,1},
+                    g::LightGraphs.AbstractGraph,
+                    opt::Int)
     index = -1
 
     freecolors = zeros(Int64, 0)
@@ -214,12 +182,12 @@ function free_colors(x, A, colors, F, g, opt)
 end
 
 """
-    least_index()
+    least_index(F::Array{Int,1}, A::Array{Int,1}, opt::Int)
 
-returns least index i such that color of vertex
-A[i] == opt (optimal color number)
+Returns least index i such that color of vertex
+A[i] is equal to `opt` (optimal color number)
 """
-function least_index(F,A,opt)
+function least_index(F::Array{Int,1}, A::Array{Int,1}, opt::Int)
     for i in eachindex(A)
         if F[A[i]] == opt
             return i
@@ -228,24 +196,25 @@ function least_index(F,A,opt)
 end
 
 """
-    uncolor_all()
+    uncolor_all(F::Array{Int,1}, A::Array{Int,1}, start::Int)
 
-uncolors all vertices A[i] where
-i >= start
+Uncolors all vertices A[i] where i is
+greater than or equal to start
 """
-function uncolor_all!(F, A, start)
+function uncolor_all!(F::Array{Int,1}, A::Array{Int,1}, start::Int)
     for i = start:length(A)
         F[A[i]] = 0
     end
 end
 
 """
-    remove_higher_colors()
+    remove_higher_colors(U::Array{Int,1}, opt::Int)
 
-remove all the colors >= opt (optimal color number)
-from the set of colors U
+Remove all the colors which are greater than or
+equal to the `opt` (optimal color number) from
+the set of colors U
 """
-function remove_higher_colors(U, opt)
+function remove_higher_colors(U::Array{Int,1}, opt::Int)
     if length(U) == 0
         return U
     end

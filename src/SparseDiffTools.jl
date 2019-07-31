ca2a2385daef5621ad4d66c2617984b7d67e6eb6
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-30 21:17:53 -0400

    import matrix_colors

--- src/SparseDiffTools.jl
+++ src/SparseDiffTools.jl
@@ -14,13 +14,14 @@ using SparseArrays, ArrayInterface
 using BlockBandedMatrices: blocksize, nblocks
 using ForwardDiff: Dual, jacobian, partials, DEFAULT_CHUNK_THRESHOLD
 
+using ArrayInterface: matrix_colors
 
 export  contract_color,
         greedy_d1,
         greedy_star1_coloring,
         greedy_star2_coloring,
         matrix2graph,
-        ArrayInterface.matrix_colors,
+        matrix_colors,
         forwarddiff_color_jacobian!,
         ForwardColorJacCache,
         auto_jacvec,auto_jacvec!,

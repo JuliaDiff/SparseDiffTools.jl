5f3e77d6294db6d11db7c9ff3c634dda9cf7e00e
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-08-07 06:39:14 +0530

    added acylic coloring

--- src/coloring/acyclic_coloring.jl
+++ src/coloring/acyclic_coloring.jl
@@ -1,4 +1,5 @@
 using LightGraphs
+using DataStructures
 
 """
         color_graph(g::LightGraphs.AbstractGraphs, :: AcyclicColoring)
@@ -14,17 +15,25 @@ function color_graph(g::LightGraphs.AbstractGraphs, ::AcyclicColoring)
     color = zeros(Int, nv(g))
     forbiddenColors = zeros(Int, nv(g))
 
+    set = DisjointSets{LightGraphs.Edge}([])
+
+    firstVisitToTree = Array{Tuple{Int, Int}, 1}(undef, ne(g))
+    firstNeighbor = Array{Tuple{Int, Int}, 1}(undef, nv(g))
+
     for v in vertices(g)
         #enforces the first condition of acyclic coloring
         for w in outneighbors(g, v)
-            forbiddenColors[color[w]] = v
+            if color[w] != 0
+                forbiddenColors[color[w]] = v
+            end
+        end
         #enforces the second condition of acyclic coloring
         for w in outneighbors(g, v)
             if color[w] != 0 #colored neighbor
-                for x in outneighbors(w)
+                for x in outneighbors(g, w)
                     if color[x] != 0 #colored x
                         if forbiddenColors[color[x]] != v
-                            prevent_cycle(v, w, x)
+                            prevent_cycle(v, w, x, g, color, forbiddenColors, firstVisitToTree, set)
                         end
                     end
                 end
@@ -36,7 +45,7 @@ function color_graph(g::LightGraphs.AbstractGraphs, ::AcyclicColoring)
         #grow star for every edge connecting colored vertices v and w
         for w in outneighbors(g, v)
             if color[w] != 0
-                growStar(v, w)
+                grow_star(v, w, g, firstNeighbor, set)
             end
         end
 
@@ -46,13 +55,15 @@ function color_graph(g::LightGraphs.AbstractGraphs, ::AcyclicColoring)
                 for x in outneighbors(g, w)
                     if color[x] != 0 && x != v
                         if color[x] == color[v]
-                            mergeTrees(v, w, x)
+                            mergeTrees(v, w, x, g, set)
                         end
                     end
                 end
             end
         end 
     end
+
+    return color
 end
 
 """
@@ -61,13 +72,18 @@ end
 """
 function prevent_cycle(v::Integer,
                        w::Integer, 
-                       x::Integer, 
+                       x::Integer,
+                       g,
+                       color, 
                        forbiddenColors::AbstractVector{<:Integer},
-                       firstVisitToTree)
-    e = Find(w, x)
-    p, q = firstVisitToTree[e]
+                       firstVisitToTree,
+                       set)
+    
+    edge = find_edge(g, w, x)
+    e = find_root(set, edge)
+    p, q = firstVisitToTree[edge_index(g, e)]
     if p != v
-        firstVisitToTree[e] = (v, w)
+        firstVisitToTree[edge_index(g, e)] = (v, w)
     else if q != w
         forbiddenColors[color[x]] = v
     end
@@ -86,8 +102,54 @@ function min_index(forbiddenColors, v)
     end
 end
 
-function growStar(v:: Integer, w::Integer)
+function grow_star(v::Integer,
+                  w::Integer,
+                  g,
+                  firstNeighbor,
+                  set)
+    edge = find_edge(g, v, w)
+    push!(set, edge)
+    p, q = firstNeighbor[color[w]]
+    if p != v
+        firstNeighbor[color[w]] = (v, w)
+    else
+        edge1 = find_edge(g, v, w)
+        edge2 = find_edge(g, p, q)
+        e1 = find_root(set, edge1)
+        e2 = find_root(set, edge2)
+        union!(set, e1, e2)
+    end
 end
 
-function mergeTrees(v:: Integer, w::Integer, x::Integer)
+function mergeTrees(v::Integer,
+                    w::Integer, 
+                    x::Integer,
+                    g,
+                    set)
+    edge1 = find_edge(g, v, w)
+    edge2 = find_edge(g, w, x)
+    e1 = find_root(set, edge1)
+    e2 = find_root(set, edge2)
+    if (e1 != e2)
+        union!(set, e1, e2)
+    end
+end
+
+function find_edge(g, v, w)
+    for e in edges(g)
+        if (src(e) == v && dst(e) == w)
+            return e
+        end
+    end
+    throw(error("$v and $w are not connected in graph g"))
+end
+
+function edge_index(g, e)
+    edge_list = collect(edges(g))
+    for i in 1:length(edge_list)
+        if edge_list[i] == e
+            return i
+        end
+    end
+    return -1
 end

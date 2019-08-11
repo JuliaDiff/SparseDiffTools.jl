02e3e8ac8d2ae2aee999d86895daf3b8220d90f8
Pankaj Mishra <pankajmishra1511@gmail.com>
2019-08-11 22:39:57 +0530

    updated runtests to include acyclic coloring

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ if GROUP == "All"
     @time @safetestset "Exact coloring via contraction" begin include("test_contraction.jl") end
     @time @safetestset "Greedy distance-1 coloring" begin include("test_greedy_d1.jl") end
     @time @safetestset "Greedy star coloring" begin include("test_greedy_star.jl") end
+    @time @safetestset "Acyclic coloring" begin include("test_acyclic.jl") end
     @time @safetestset "Matrix to graph conversion" begin include("test_matrix2graph.jl") end
     @time @safetestset "AD using colorvec vector" begin include("test_ad.jl") end
     @time @safetestset "Integration test" begin include("test_integration.jl") end

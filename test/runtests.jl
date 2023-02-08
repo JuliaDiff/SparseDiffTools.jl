179688018e71da7c2773530f2e72bbd68f60bb5d
Vedant Puri <vedantpuri@gmail.com>
2023-02-08 08:30:04 -0500

    uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,16 +12,16 @@ function activate_gpu_env()
 end
 
 if GROUP == "All"
-    #@time @safetestset "Exact coloring via contraction" begin include("test_contraction.jl") end
-    #@time @safetestset "Greedy distance-1 coloring" begin include("test_greedy_d1.jl") end
-    #@time @safetestset "Greedy star coloring" begin include("test_greedy_star.jl") end
-    #@time @safetestset "Acyclic coloring" begin include("test_acyclic.jl") end
-    #@time @safetestset "Matrix to graph conversion" begin include("test_matrix2graph.jl") end
-    #@time @safetestset "AD using colorvec vector" begin include("test_ad.jl") end
-    #@time @safetestset "Hessian colorvecs" begin include("test_sparse_hessian.jl") end
-    #@time @safetestset "Integration test" begin include("test_integration.jl") end
-    #@time @safetestset "Special matrices" begin include("test_specialmatrices.jl") end
-    #@time @safetestset "Jac Vecs and Hes Vecs" begin include("test_jaches_products.jl") end
+    @time @safetestset "Exact coloring via contraction" begin include("test_contraction.jl") end
+    @time @safetestset "Greedy distance-1 coloring" begin include("test_greedy_d1.jl") end
+    @time @safetestset "Greedy star coloring" begin include("test_greedy_star.jl") end
+    @time @safetestset "Acyclic coloring" begin include("test_acyclic.jl") end
+    @time @safetestset "Matrix to graph conversion" begin include("test_matrix2graph.jl") end
+    @time @safetestset "AD using colorvec vector" begin include("test_ad.jl") end
+    @time @safetestset "Hessian colorvecs" begin include("test_sparse_hessian.jl") end
+    @time @safetestset "Integration test" begin include("test_integration.jl") end
+    @time @safetestset "Special matrices" begin include("test_specialmatrices.jl") end
+    @time @safetestset "Jac Vecs and Hes Vecs" begin include("test_jaches_products.jl") end
     @time @safetestset "Operator tests" begin include("test_ops.jl") end
 
 end

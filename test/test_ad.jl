df9b45afeb8a8332635ce2fe5e8b9360dd965ce8
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-02-19 08:14:03 -0500

    remove other arrayinterfaces in tests

--- test/test_ad.jl
+++ test/test_ad.jl
@@ -2,9 +2,9 @@ using SparseDiffTools
 using ForwardDiff: Dual, jacobian, value
 using SparseArrays, Test
 using LinearAlgebra
-using BlockBandedMatrices, ArrayInterfaceBlockBandedMatrices
-using BandedMatrices, ArrayInterfaceBandedMatrices
-using StaticArrays#, StaticArrayInterface
+using BlockBandedMatrices
+using BandedMatrices
+using StaticArrays
 
 fcalls = 0
 function f(dx, x)
